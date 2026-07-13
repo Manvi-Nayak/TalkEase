6cf568cb6ccf3d753ef190f4b3ead393b8b7a1d3
Manvi Nayak <manvi2605nayak@gmail.com>
2026-07-13 20:52:12 +0530

    Modify Firebase credentials file path
    Updated path to Firebase credentials file for better portability.

--- firebase_config.py
+++ firebase_config.py
@@ -2,5 +2,5 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("C:/Users/hp/Desktop/Project/Talk-Ease/talkease-d404e-firebase-adminsdk-fbsvc-899cf3b3c0.json")
+    cred = credentials.Certificate("path to your json file")
     firebase_admin.initialize_app(cred)
